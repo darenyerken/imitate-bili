9517458d2e8d0aa0c181b7071539d2afd2d85f67
daren <1768929241@qq.com>
2023-12-07 14:53:02 +0800

    feat: pnpm and legacy

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,16 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    legacy({
+      polyfills: ['es.promise.finally', 'es/map', 'es/set'],
+      modernPolyfills: ['es.promise.finally'],
+    }),
   ],
   resolve: {
     alias: {
