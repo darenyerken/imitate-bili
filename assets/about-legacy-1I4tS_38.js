f72f3f0054f920cfbc981b30f6e599cbd16987f3
daren <1768929241@qq.com>
2023-12-19 20:30:40 +0800

    chore: chore

--- assets/about-legacy-1I4tS_38.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-N3tR5Nbb.js"],(function(e,t){"use strict";var n,r,u;return{setters:[function(e){n=e._,r=e.x,u=e.y}],execute:function(){e("default",n({},[["render",function(e,t){return r(),u("h3",null,"about page")}]]))}}}));
---
+++ assets/about-legacy-yiHcQU6C.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-ZseRjmA0.js"],(function(e,t){"use strict";var n,r,u;return{setters:[function(e){n=e._,r=e.x,u=e.y}],execute:function(){e("default",n({},[["render",function(e,t){return r(),u("h3",null,"about page")}]]))}}}));
